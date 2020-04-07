64578bc7e6100449bc678ec077611778cae66a53
= <roliveirasv@gmail.com>
2020-04-07 18:02:33 -0300

    Construção da Rota GET/POST e Método GET e POST do controller

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 var HomeController = require("../app/controllers/home_controller");
+var AcoesUsuariosController = require("../app/controllers/acoes_usuarios_controller")
 
 
 router.get("/",HomeController.index);
+router.get("/acoes-usuarios.json",AcoesUsuariosController.index)
+router.post("/acoes-usuarios.json",AcoesUsuariosController.create)
 
 
 module.exports = router;
