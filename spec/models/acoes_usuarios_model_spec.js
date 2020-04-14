6015df3e6975fe81ba455e9fd5ecf73b30ea47e2
= <roliveirasv@gmail.com>
2020-04-14 19:15:57 -0300

    Teste model - retornar true se model AcoesUsuarios foi criado

--- spec/models/acoes_usuarios_model_spec.js
+++ spec/models/acoes_usuarios_model_spec.js
@@ -36,12 +36,18 @@ describe("AcoesUsuario", () => {
 
   describe("Deve retornar se o modelo de AcoesUsuario", () => {
     it('retornou o valor investido', () => {
-      let acao = AcoesUsuario.findOne({}).exec( async(err, acao) => {
+        AcoesUsuario.findOne({}).exec( async(err, acao) => {
         let nome = `teste ${new Date().getTime()}`;
-        let valorInvestido = await AcoesUsuario.valorInvestidoPorAcao(acao.codigo_usuario, acao.codigo_acao);
+        let valorInvestido = await AcoesUsuario.valorInvestidoPorAcao(acao.codigo_usuario, acao.codigo_acao)  ;
         expect(valorInvestido.total != null && valorInvestido.total != undefined).toBe(true);
         expect(valorInvestido.acao != null && valorInvestido.acao != undefined).toBe(true);
       })
     });
   })
+   
+   it('Foi criado',()=>{AcoesUsuario.create({codigo_usuario: "1",codigo_acao:"ZM007",valor_investido:45,percentual:15},(err,res)=>{
+     console.log(res);
+     expect(err !== undefined ||err !== null).toBe(true)})
+  })
+
 })
